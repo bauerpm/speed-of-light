d3d312c248f6506afd0356f3226b67e91e8ae85c
Mike Bauer <mikeb@parsed.io>
2020-11-05 20:39:34 -0500

    adding text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { faMeteor, faStarOfLife } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGlobeAfrica,
+  faMeteor,
+  faStarOfLife,
+  faSun,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGalacticRepublic } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
 import { Animation } from './components/animation.component'
 
@@ -7,25 +13,53 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <p>How Slow is the The Speed of Light?</p>
+        <p>How Fast is the The Speed of Light?</p>
       </div>
-      <Animation icon={faStarOfLife} className="Animation-icon" />
-      <Animation icon={faMeteor} className="Animation-icon-earth" />
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div className="rowContainer">
+        <div>
+          <div className="container">
+            <div className="textcontainer">
+              <p className="label">Light</p>
+              <p className="label">299,792,558 m/s</p>
+            </div>
+            <Animation icon={faStarOfLife} className="Animation-icon" />
+          </div>
+          <div className="container">
+            <div className="textcontainer">
+              <p className="label">The Milky Way</p>
+              <p className="label">581,152 m/s</p>
+            </div>
+            <Animation
+              icon={faGalacticRepublic}
+              className="Animation-icon-milkyway"
+            />
+          </div>
+          <div className="container">
+            <div className="textcontainer">
+              <p className="label">The Solar System</p>
+              <p className="label">200,000 m/s</p>
+            </div>
+            <Animation icon={faSun} className="Animation-icon-solarsystem" />
+          </div>
+          <div className="container">
+            <div className="textcontainer">
+              <p className="label">Comet 2I/Borisov</p>
+              <p className="label">48,611 m/s</p>
+            </div>
+            <Animation icon={faMeteor} className="Animation-icon-comet" />
+          </div>
+          <div className="container">
+            <div className="textcontainer">
+              <p className="label">The Earth</p>
+              <p className="label">30,000 m/s</p>
+            </div>
+            <Animation icon={faGlobeAfrica} className="Animation-icon-earth" />
+          </div>
+        </div>
+        <div className="paragraph">
+          <p className="label">lj;lskjfds</p>
+        </div>
+      </div>
     </div>
   )
 }
